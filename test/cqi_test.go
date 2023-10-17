67e89bde9e9496c2ceba1e73af951ea242e08a7f
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2023-10-17 14:45:04 +0900

    add error control (#8)
    for cqi and numerology function

--- test/cqi_test.go
+++ test/cqi_test.go
@@ -1,6 +1,7 @@
 package nrConversion
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/zulfadlizainal/goran/pkg/nrConversion"
@@ -11,18 +12,24 @@ func TestQCIToType(t *testing.T) {
 		qci int
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
 	}{
-		{"5QI 1", args{1}, "GBR"},
-		{"5QI 2", args{2}, "GBR"},
-		{"5QI 3", args{3}, "GBR"},
-		{"5QI -1 (Out of Range)", args{-1}, "-1"},
+		{"5QI 1", args{1}, "GBR", false},
+		{"5QI 2", args{2}, "GBR", false},
+		{"5QI 3", args{3}, "GBR", false},
+		{"5QI -1 (Out of Range)", args{-1}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := nrConversion.QCIToType(tt.args.qci); got != tt.want {
+			got, err := nrConversion.QCIToType(tt.args.qci)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QCIToType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("QCIToType() = %v, want %v", got, tt.want)
 			}
 		})
@@ -34,18 +41,24 @@ func TestQCIToPriority(t *testing.T) {
 		qci int
 	}
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
 	}{
-		{"5QI 1", args{1}, 2.0},
-		{"5QI 2", args{2}, 4.0},
-		{"5QI 3", args{3}, 3.0},
-		{"5QI -1 (Out of Range)", args{-1}, -1.0},
+		{"5QI 1", args{1}, 2.0, false},
+		{"5QI 2", args{2}, 4.0, false},
+		{"5QI 3", args{3}, 3.0, false},
+		{"5QI -1 (Out of Range)", args{-1}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := nrConversion.QCIToPriority(tt.args.qci); got != tt.want {
+			got, err := nrConversion.QCIToPriority(tt.args.qci)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QCIToPriority() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("QCIToPriority() = %v, want %v", got, tt.want)
 			}
 		})
@@ -57,18 +70,24 @@ func TestQCIToPacketDelay(t *testing.T) {
 		qci int
 	}
 	tests := []struct {
-		name string
-		args args
-		want int
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
 	}{
-		{"5QI 1", args{1}, 100},
-		{"5QI 2", args{2}, 150},
-		{"5QI 3", args{3}, 50},
-		{"5QI -1 (Out of Range)", args{-1}, -1},
+		{"5QI 1", args{1}, 100, false},
+		{"5QI 2", args{2}, 150, false},
+		{"5QI 3", args{3}, 50, false},
+		{"5QI -1 (Out of Range)", args{-1}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := nrConversion.QCIToPacketDelay(tt.args.qci); got != tt.want {
+			got, err := nrConversion.QCIToPacketDelay(tt.args.qci)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QCIToPacketDelay() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("QCIToPacketDelay() = %v, want %v", got, tt.want)
 			}
 		})
@@ -80,18 +99,24 @@ func TestQCIToPacketLoss(t *testing.T) {
 		qci int
 	}
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
 	}{
-		{"5QI 1", args{1}, 0.01},
-		{"5QI 2", args{2}, 0.001},
-		{"5QI 3", args{3}, 0.001},
-		{"5QI -1 (Out of Range)", args{-1}, -1.0},
+		{"5QI 1", args{1}, 0.01, false},
+		{"5QI 2", args{2}, 0.001, false},
+		{"5QI 3", args{3}, 0.001, false},
+		{"5QI -1 (Out of Range)", args{-1}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := nrConversion.QCIToPacketLoss(tt.args.qci); got != tt.want {
+			got, err := nrConversion.QCIToPacketLoss(tt.args.qci)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QCIToPacketLoss() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("QCIToPacketLoss() = %v, want %v", got, tt.want)
 			}
 		})
